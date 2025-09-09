25ff49bf379f5c33ad59a8c3abe61b9b505ac12f
Alijonovaward <Alijonovaward@gmail.com>
2025-09-09 17:24:50 +0500

    a

--- qazo/settings.py
+++ qazo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1sm$vm-*%uw!utn#^gu*-!mzy+2lyostnkpx@%3dz3-k591nwm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
