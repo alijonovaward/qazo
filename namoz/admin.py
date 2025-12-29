4b65d24e56ab90e11e59d9560b1573487ce9c2a2
Alijonovaward <Alijonovaward@gmail.com>
2025-12-29 11:52:52 +0500

    namoz action ham admin panelga qoshildi

--- namoz/admin.py
+++ namoz/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Namoz
+from .models import Namoz, NamozAction
 
 # Register your models here.
 admin.site.register(Namoz)
+
+@admin.register(NamozAction)
+class NamozActionAdmin(admin.ModelAdmin):
+    search_fields = ['user']
+    list_display = ['user', 'prayer', 'action', 'old_value', 'new_value', 'created_at']
+    list_filter = ['user', 'prayer', 'action', 'created_at']
