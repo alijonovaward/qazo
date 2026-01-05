5afefeeaf91fb5c730a5aa22e167c5decac0d1dd
Alijonovaward <Alijonovaward@gmail.com>
2026-01-05 11:00:04 +0500

    follow modeli olib tashlandi

--- follow/admin.py
+++ follow/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
 
-from follow.models import Follow
 
-
-# Register your models here.
-@admin.register(Follow)
-class FollowAdmin(admin.ModelAdmin):
-    list_display = ('following', 'follower', 'status')
-    list_filter = ('following', 'follower', 'status')
-    search_fields = ('following', 'follower')
--- follow/views.py
+++ follow/views.py
@@ -7,15 +7,3 @@ def friends(request):
 
     context = {'users':users}
     return render(request, 'follow/userlist.html', context)
-
-def get_followers(request):
-    user = request.user
-
-    followers_users = [
-        f.follower
-        for f in user.followers.filter(status='accepted')
-    ]
-
-    context = {'users':followers_users}
-
-    return render(request, 'follow/userlist.html', context)
