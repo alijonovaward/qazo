3d92f0cc0250b6a8013e940c23d7b3803da53051
Alijonovaward <Alijonovaward@gmail.com>
2026-01-05 11:46:50 +0500

    a

--- follow/views.py
+++ follow/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import Permission
 
 # Create your views here.
 def friends(request):
     users = User.objects.all()
 
     context = {'users':users}
+    return render(request, 'follow/userlist.html', context)
+
+def get_followers(request):
+    permissions = Permission.objects.filter(receiver=request.user)
+    users = [p.sender for p in permissions]
+
+    context = {'users':users}
+
     return render(request, 'follow/userlist.html', context)
