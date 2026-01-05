49c3f89c7a57ef0f3cae64bed79dd865b0ae20c0
Alijonovaward <Alijonovaward@gmail.com>
2026-01-05 11:10:31 +0500

    permission modeli qoshildi

--- follow/admin.py
+++ follow/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Permission
 
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'status')
+    search_fields = ('sender', 'receiver')
+    list_filter = ('sender', 'receiver', 'status')
+
+    class Meta:
+        unique_together = (('sender', 'receiver'),)
--- follow/models.py
+++ follow/models.py
@@ -2,3 +2,19 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Permission(models.Model):
+    STATUS_CHOICES = (
+    ('pending', 'pending'),
+    ('accepted', 'accepted'),
+    ('rejected', 'rejected'),
+    )
+
+    sender = models.ForeignKey(User, related_name='sender', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='receiver', on_delete=models.CASCADE)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=50)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.sender} -> {self.receiver}'
